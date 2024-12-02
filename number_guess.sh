6c27b91194a260e34e56142c7035b947aff2526c
Agustin Varas <Agustin.Varas@outlook.com.ar>
2024-12-02 15:06:13 +0000

    feat: Added first version of player validation with the DB

--- number_guess.sh
+++ number_guess.sh
@@ -5,8 +5,13 @@ PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 echo -e "\nEnter your username:"
 read USER_NAME
 
-#if not found
-echo -e "\nWelcome, <username>! It looks like this is your first time here."
+USER_LIST_RESULT=$($PSQL "SELECT username FROM players WHERE username = '$USER_NAME' " )
 
-#if found
-echo -e "\nWelcome back, <username>! You have played <games_played> games, and your best game took <best_game> guesses"
+if [[ -z $USER_LIST_RESULT ]]
+then
+  #if not found
+  echo -e "\nWelcome, <username>! It looks like this is your first time here."
+else
+  #if found
+  echo -e "\nWelcome back, <username>! You have played <games_played> games, and your best game took <best_game> guesses"
+fi
