f197a8ac1089d0c859691600538bc8872d93ae12
Agustin Varas <Agustin.Varas@outlook.com.ar>
2024-12-02 14:27:11 +0000

    feat: added first two prompts for the game

--- number_guess.sh
+++ number_guess.sh
@@ -2,3 +2,8 @@
 
 PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
 
+echo -e "\nEnter your username:"
+READ USER_NAME
+
+#if found
+echo -e "\nWelcome back, <username>! You have played <games_played> games, and your best game took <best_game> guesses"
