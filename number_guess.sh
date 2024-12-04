0e350ccd0175b2ae1a4137c7ba0595500f783b00
Agustin Varas <Agustin.Varas@outlook.com.ar>
2024-12-04 12:40:59 +0000

    feat: Added guess input validations
    feat: Added handling for when the guess is lower than the random number
    
    feat: Added handling for when the guess is higher than the random number
    
    feat: Added handling for when the guess is equal to the random number

--- number_guess.sh
+++ number_guess.sh
@@ -6,6 +6,7 @@ MAX=1000
 MIN=1
 
 SECRET_NUMBER=$(($RANDOM%($MAX-$MIN)+$MIN))
+echo $SECRET_NUMBER
 
 echo -e "\nEnter your username:"
 read USER_NAME
@@ -18,6 +19,28 @@ then
   echo -e "\nWelcome, <username>! It looks like this is your first time here."
 else
   #if found
-  echo -e "\nWelcome back, <username>! You have played <games_played> games, and your best game took <best_game> guesses"
+  echo -e "\nWelcome back, $USER_LIST_RESULT! You have played <games_played> games, and your best game took <best_game> guesses"
+  echo -e "\nGuess the secret number between 1 and 1000:"
 fi
 read PLAYER_GUESS
+
+if [[ $PLAYER_GUESS =~ ^[0-9]+$ ]]
+then
+  
+  #When the guess is higher
+  if [[ $PLAYER_GUESS > $SECRET_NUMBER ]]
+  then
+    echo -e "\nIt's lower than that, guess again:"
+  fi
+  
+  #When the guess is lower
+  if [[ $PLAYER_GUESS < $SECRET_NUMBER ]]
+  then
+    echo -e "\nIt's higher than that, guess again:"
+  fi
+
+  if [[ $PLAYER_GUESS == $SECRET_NUMBER ]] 
+  then
+    echo -e "\nYou guessed it in <number_of_guesses> tries. The secret number was $SECRET_NUMBER. Nice job!"
+  fi
+fi
