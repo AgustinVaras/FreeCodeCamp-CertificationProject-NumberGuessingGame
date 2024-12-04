00636b794892bbf75053d5aa9e277d48602ed98c
Agustin Varas <Agustin.Varas@outlook.com.ar>
2024-12-04 15:36:41 +0000

    refactor: refactored prompts to be equal to what is asked in the project tasks

--- number_guess.sh
+++ number_guess.sh
@@ -6,7 +6,7 @@ MAX=1000
 MIN=1
 
 SECRET_NUMBER=$(($RANDOM%($MAX-$MIN)+$MIN))
-echo $SECRET_NUMBER
+# echo $SECRET_NUMBER
 
 echo -e "\nEnter your username:"
 read USER_NAME
@@ -60,7 +60,7 @@ else
   #if found
   echo $USER_LIST_RESULT | while read USERNAME BAR GAMES_COUNT BAR BEST_GAME
   do
-    echo -e "\nWelcome back, $USERNAME! You have played $GAMES_COUNT games, and your best game took $BEST_GAME guesses"
+    echo "Welcome back, $USERNAME! You have played $GAMES_COUNT games, and your best game took $BEST_GAME guesses."
   done
   echo -e "\nGuess the secret number between 1 and 1000:"
 fi
@@ -70,7 +70,7 @@ TRYS=0;
 while [[ $PLAYER_GUESS != $SECRET_NUMBER ]]
 do
   (( TRYS++ ))
-  echo -e "\nTrys: $TRYS"
+  # echo -e "\nTrys: $TRYS"
   read PLAYER_GUESS
 
   if [[ $PLAYER_GUESS =~ ^[0-9]+$ ]]
@@ -88,10 +88,10 @@ do
       echo -e "\nIt's higher than that, guess again:"
     fi
 
-    if [[ $PLAYER_GUESS == $SECRET_NUMBER ]] 
-    then
-      echo -e "\nYou guessed it in $TRYS tries. The secret number was $SECRET_NUMBER. Nice job!"
-    fi
+    # if [[ $PLAYER_GUESS == $SECRET_NUMBER ]] 
+    # then
+      
+    # fi
   else
     echo -e "\nThat is not an integer, guess again:"
   fi
@@ -101,3 +101,4 @@ done
 PLAYER_ID=$($PSQL "SELECT player_id FROM players WHERE username = '$USER_NAME'")
 
 INSERT_GAME_RESULT=$($PSQL "INSERT INTO games(player_id, guesses) VALUES($PLAYER_ID, $TRYS)" )
+echo "You guessed it in $TRYS tries. The secret number was $SECRET_NUMBER. Nice job!"
